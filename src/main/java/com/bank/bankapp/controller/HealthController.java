f7f8a4aca0896e29d257684b76c95ebe3f45536d
Ubuntu <ubuntu@ip-172-31-21-209.ap-south-1.compute.internal>
2026-02-08 16:52:06 +0000

    Add health endpoint

---
+++ src/main/java/com/bank/bankapp/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.bank.bankapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "BankApp is running";
+    }
+}
